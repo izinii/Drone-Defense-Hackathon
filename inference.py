fd78449dda34bfc7907d3aff0e10719bd6993556
Ilan Zini <ilan.zini@thalesgroup.com>
2025-11-03 16:06:09 +0000

    inference works for YOLO models

--- inference.py
+++ inference.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 # Train the model
-#results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
+results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
 
 # Run inference on an image
-results = model("images/img2.jpg", show=True, save=True) # return a Result object
+#results = model("images/img3.jpg", show=True, save=True) # return a Result object

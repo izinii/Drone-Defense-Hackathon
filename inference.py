18884442e3151de14152f69faa3a9ee0b3e147ee
Ilan Zini <ilan.zini@thalesgroup.com>
2025-11-04 13:24:09 +0000

    yolo11n model trained on VisDrone dataset

--- inference.py
+++ inference.py
@@ -1,13 +1,18 @@
 from ultralytics import YOLO
 
+"""
 # Load a model
 model = YOLO("yolo11n.pt")  # load a pretrained model (recommended for training)
 
-# Display model information
-model.info()
-
 # Train the model
-#results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
+results = model.train(data="VisDrone.yaml", epochs=100, imgsz=640)
 
 # Run inference on an image
-#results = model("images/img3.jpg", show=True, save=True) # return a Result object
+results = model("images/img2.jpg", show=True, save=True) # return a Result object
+"""
+
+# Display model information
+#print("\nModel info:", model.info(), "\n\n")
+
+model = YOLO("yolo11n_visdrone.pt")
+results = model("images/", save=True)
